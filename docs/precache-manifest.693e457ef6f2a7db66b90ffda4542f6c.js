c5e85df3be2bfa99e1fd940723ff67b09cd24389
mave108 <rakeshkumar8900@gmail.com>
2019-02-15 11:04:16 +0530

    production build

---
+++ docs/precache-manifest.693e457ef6f2a7db66b90ffda4542f6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b02663f790d7c04dd4b",
+    "url": "/react-cart/static/js/runtime~main.5b02663f.js"
+  },
+  {
+    "revision": "a51c84e848eb352811be",
+    "url": "/react-cart/static/js/main.a51c84e8.chunk.js"
+  },
+  {
+    "revision": "7f9812788a3adf0244e4",
+    "url": "/react-cart/static/js/2.7f981278.chunk.js"
+  },
+  {
+    "revision": "a51c84e848eb352811be",
+    "url": "/react-cart/static/css/main.8c4d2c92.chunk.css"
+  },
+  {
+    "revision": "7f9812788a3adf0244e4",
+    "url": "/react-cart/static/css/2.a16ce26a.chunk.css"
+  },
+  {
+    "revision": "3c3cb190c1a00631096aeb8122b5264c",
+    "url": "/react-cart/index.html"
+  }
+];
